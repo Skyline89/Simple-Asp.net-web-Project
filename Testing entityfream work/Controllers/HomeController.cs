59164c6c947ae647e1a6f0166e2241ca604c9f78
Skyline89 <aleko.mesarkishvili@gmail.com>
2019-12-04 17:08:53 +0400

    Added ManagerUserPartial method to Home controller and view

--- Testing entityfream work/Controllers/HomeController.cs
+++ Testing entityfream work/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Testing_entityfream_work.Models.EntityManager;
+using Testing_entityfream_work.Models.ViewModel;
 using Testing_entityfream_work.Security;
 
 namespace Testing_entityfream_work.Controllers
@@ -26,6 +28,18 @@ namespace Testing_entityfream_work.Controllers
         }
         public ActionResult UnAuthorized()
         {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManagerUserPartial()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager Um = new UserManager();
+                UserDataView Udv = Um.GetUserDataView(loginName);
+                return PartialView(Udv);
+            }
             return View();
         }
     }     
